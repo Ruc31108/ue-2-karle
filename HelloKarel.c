75fdf51f1cacec3d9180782c5393e3e82a4531ce
Christian Rusch <christian.rusch@st.oth-regensburg.de>
2018-10-05 22:01:19 +0200

    [INIT] project

---
+++ HelloKarel.c
@@ -0,0 +1,60 @@
+/*
+ * File: HelloKarel.c
+ * --------------------------------
+ * The HelloKarel program is a simple
+ * program to demonstrate a minimal working
+ * Karel example.
+ * 
+ */
+
+#include "karel.h"
+/*
+ * Truns Karel 90 degress to the Right
+ */
+void turnRight(){
+    for (int turnRightCounter = 0; turnRightCounter < 3; ++turnRightCounter) {
+        turnLeft();
+    }
+}
+
+void setup() {
+    loadWorld("damagedPillar1");
+}
+
+void bigStep(){
+    for (int moveCounter = 0; moveCounter < 4; ++moveCounter) {
+        move();
+    }
+}
+
+void checkForBeep() {
+    // Kein # vorhanden
+    if (noBeepersPresent()){
+        // Hinlegen
+        putBeeper();
+    }
+}
+
+void build(){
+    checkForBeep();
+    if(!frontIsBlocked()) {
+        bigStep();
+    } else {
+        if (facingEast()) {
+            turnLeft();
+            move();
+            turnLeft();
+        } else if (facingWest()) {
+            turnRight();
+            move();
+            turnRight();
+        }
+    }
+}
+
+void run() {
+    while(!((frontIsBlocked() && leftIsBlocked() && facingEast()) || (frontIsBlocked() && rightIsBlocked() && facingWest()))) {
+        build();
+    }
+}
+
